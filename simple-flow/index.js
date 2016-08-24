0af3e6c8979f33b8abff7f2b8ae3984b35ca0f56
Isaah Forrest <isaah@dropteched.com>
2016-08-24 11:17:00 -0500

    simple flow setup

---
+++ simple-flow/index.js
@@ -0,0 +1,13 @@
+/* @flow */
+
+function foo(x) {
+  return x * 10;
+}
+
+foo("Hello, world!");
+
+var str: number = 1;
+console.log(str);
+
+var fixedStr: string = 'hello world!';
+console.log(fixedStr);
